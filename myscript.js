174b068870551af377fdc5b8fd701e2309996adf
Darren Arichandran <d.arichandran@gmail.com>
2024-04-03 23:07:38 +0100

    basic boilrplate with class and js file completed

---
+++ myscript.js
@@ -0,0 +1,31 @@
+
+
+let number1 = a;
+let number2 = b;
+let operator = x;
+
+function operate(number1, operator, number2) {
+
+    function sum(a,b) {
+        alert a+b;
+    }
+    function subtract(a,b) {
+        alert a-b;
+    }
+    function multiply (a,b) {
+        alert a*b;
+    }
+    function divide(a,b){
+        alert a/b;
+    }
+
+    if (operator === "+") {
+        function sum(number1,number2)
+    } else if (operator ==="-") {
+        function subtract(number1, number2)
+    } else if (operator === "*") {
+        function multiply(number1, number2)
+    } else if (operator === "/") {
+        function divide(number1, number2)
+    }
+}
